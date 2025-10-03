a8600bb944df7646986316089f8d299dcfb08ca3
Mateo A <mateo@MacBookAir.lan>
2025-10-02 23:11:04 -0700

    commited to repo using autogit

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-function sendMail(){
-    
-    var params = {
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        message: document.getElementById('message').value,
+function sendMail(event) {
+    event.preventDefault(); // prevent page reload
+
+    const params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
     };
 
-const serviceID = "service_68oovjk";
-const templateID = "template_w9qv3ey";
+    const serviceID = "service_68oovjk";
+    const templateID = "template_w9qv3ey";
 
-emailjs.send(serviceID, templateID, params)
-.then(
-    res => {
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("message").value = "";
-        console.log(res);
-        alert("Email Sent Successfully! I will get back to you shortly.");
-
-})
-.catch((err) => console.log(err));
-
+    emailjs.send(serviceID, templateID, params)
+        .then(res => {
+            console.log("SUCCESS", res);
+            document.getElementById("contact-form").reset();
+            document.getElementById("msg").innerText = "Message sent successfully!";
+            document.getElementById("msg").style.color = "green";
+        })
+        .catch(err => {
+            console.error("FAILED", err);
+            document.getElementById("msg").innerText = "Failed to send. Please try again.";
+            document.getElementById("msg").style.color = "red";
+        });
 }
+
+
+
+
+
+
+
